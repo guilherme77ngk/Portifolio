00b2b5bf6d4de2f080b1d67a46bfe9edd3211545
Guilherme Barbosa <148016220+guilherme77ngk@users.noreply.github.com>
2024-01-09 16:18:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//efeito do botão voltar ao topo na página inicial
+
+function topo(){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:'smooth'
+        }
+    )
+}
